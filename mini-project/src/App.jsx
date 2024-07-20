cf3957eb666da8d79276f9de0c48a59d80f6c385
Harsh Rajput <harshrajput30411@gmail.com>
2024-07-20 15:15:44 +0530

    first commit

---
+++ mini-project/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Weather from './weather';
+
+
+function App() {
+  // const [count, setCount] = useState(0);
+
+  let handleClick = () =>{
+    console.log("button click");
+  }
+
+return (
+<div className='root'>
+<Weather />
+</div>
+ )
+}
+
+export default App

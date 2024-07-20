94eee1e9e8df9d36be028c6a428ddd818ff74e04
Harsh Rajput <harshrajput30411@gmail.com>
2024-07-20 15:32:42 +0530

    update file

--- mini-project/vite.config.js
+++ mini-project/vite.config.js
@@ -1,7 +1,25 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/main.js'),
+        vendor: resolve(__dirname, 'src/vendor.js')
+      },
+      output: {
+        format: 'es',
+        dir: 'dist',
+        sourcemap: true,
+        manualChunks: {
+          vendor: ['react', 'react-dom']
+        }
+      },
+      external: ['react', 'react-dom']
+    }
+  }
 })
+
